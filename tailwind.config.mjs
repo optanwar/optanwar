f1989fb9e8b03c2d43c3b1f8afeb8ab4849d6d40
OPTANWAR2001 <optanwar10@gmail.com>
2024-01-23 18:33:32 +0530

    feat home

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          dark_gray: "#101010", // pinkish-red
+          red: "#ff0008", // pinkish-red
+          gray: "#181818", // dar
+        },
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+      backgroundImage: {
+        // home: "url('/bg-images/home_bg_img_main.svg')",
+      },
+    },
+  },
+  plugins: [],
+};
