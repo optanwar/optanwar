04075da467a7e96cdf3b7de3749d74fcd756104a
OPTANWAR2001 <optanwar10@gmail.com>
2024-02-09 01:33:18 +0530

    add dark mode button

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "node_modules/preline/dist/*.js",
+  ],
   theme: {
     extend: {
       colors: {
@@ -18,5 +21,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms"), require("preline/plugin")],
 };
