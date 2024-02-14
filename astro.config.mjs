ad55b0cb878d3fa95a14852cd331e5b47238b74f
OPTANWAR2001 <optanwar10@gmail.com>
2024-02-15 03:08:21 +0530

    add image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), astroImageTools],
 
 });
+
+
